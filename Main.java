69c98edbf0d2c89057a974ec0c39ba2457a5d6fe
Grassclipper1 <117752579+Grassclipper1@users.noreply.github.com>
2022-11-09 13:45:08 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+   
+    System.out.println("Hello GitHub checkpoint!");
+    System.out.println("Let us hope this works!");
+    
+    
+  }
+}
